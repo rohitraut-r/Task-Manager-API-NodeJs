50916ad076323bc5e6f91c8f02e768f0d0d7af71
Rohit <rautrohit.rr123@gmail.com>
2024-05-16 19:18:10 +0545

    Add Data Sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,10 +23,15 @@ router.post('/tasks',auth , async (req, res)=>{
 
 router.get('/tasks', auth, async (req, res)=>{
     const match = {};
+    const sort = {};
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
     }
     
+    if(req.query.sortBy){
+        const part = req.query.sortBy.split(':');
+        sort[part[0]] = part[1] === 'desc' ? -1 : 1;
+    }
 
     try {
         // const tasks = await Task.find(match);
@@ -35,8 +40,10 @@ router.get('/tasks', auth, async (req, res)=>{
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip) 
+                skip: parseInt(req.query.skip) ,
+                sort,
             }
+            
         })
         res.send(req.user.tasks);
     } catch (error) {
